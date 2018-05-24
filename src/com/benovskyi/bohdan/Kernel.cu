93dd9dc7cc0ad13bd1447181427018769a90fe0f
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-05-24 18:17:23 +0300

    change arguments, add size of array

--- src/com/benovskyi/bohdan/Kernel.cu
+++ src/com/benovskyi/bohdan/Kernel.cu
@@ -1,7 +1,7 @@
 extern "C"
-__global__ void matrixAdd(int *a, int *b, int *c) {
+__global__ void matrixAdd(int n, int *a, int *b, int *c) {
 	int i = threadIdx.x;
 	int j = threadIdx.y;
-	int index = i + j * 5;
+	int index = i + j * n;
 	c[index] = (a[index] + b[index]) * 5 + 8;
 }
