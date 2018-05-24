9f9e6580e9b848554bffa8447503cae83f50f1b2
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-05-24 17:10:01 +0300

    preparePtxFile(String cuFileName)   and     toByteArray(InputStream inputStream) functions, also prepare two matrix

--- src/com/benovskyi/bohdan/Runner.java
+++ src/com/benovskyi/bohdan/Runner.java
@@ -1,10 +1,171 @@
 package com.benovskyi.bohdan;
 
+import static jcuda.driver.JCudaDriver.cuCtxCreate;
+import static jcuda.driver.JCudaDriver.cuDeviceGet;
+import static jcuda.driver.JCudaDriver.cuInit;
+import static jcuda.driver.JCudaDriver.cuModuleGetFunction;
+import static jcuda.driver.JCudaDriver.cuModuleLoad;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+
+import jcuda.driver.CUcontext;
+import jcuda.driver.CUdevice;
+import jcuda.driver.CUfunction;
+import jcuda.driver.CUmodule;
+import jcuda.driver.JCudaDriver;
+
 public class Runner {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		// Enable exceptions and omit all subsequent error checks
+        JCudaDriver.setExceptionsEnabled(true);
+        
+        String ptxFileName = preparePtxFile("D:\\Projects\\JCUDA_lab\\src\\com\\benovskyi\\bohdan\\Kernel.cu");
+        
+        // Initialize the driver and create a context for the first device.
+        cuInit(0);
+        CUdevice device = new CUdevice();
+        cuDeviceGet(device, 0);
+        CUcontext context = new CUcontext();
+        cuCtxCreate(context, 0, device);
+        
+        // Load the ptx file.
+        CUmodule module = new CUmodule();
+        cuModuleLoad(module, ptxFileName);
+        
+        // Obtain a function pointer to the "add" function.
+        CUfunction function = new CUfunction();
+        cuModuleGetFunction(function, module, "matrixAdd");
+        
+        int h_a[][] = new int[5][5];
+        int h_b[][] = new int[5][5];
+        int h_c[][] = new int[5][5];
+        
+        int size = 5*5;
+        Random rand = new Random();
+        
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < 5; j++) {
+            	h_a[i][j] = rand.nextInt(10);
+            	h_b[i][j] = rand.nextInt(10);
+            }
+        }
+        
+        System.out.println("Matrix A --->");
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < 5; j++) {
+            	System.out.print(h_a[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        
+        System.out.println("Matrix B --->");
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < 5; j++) {
+            	System.out.print(h_b[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        
+        
 
 	}
+	
+	/**
+     * The extension of the given file name is replaced with "ptx".
+     * If the file with the resulting name does not exist, it is
+     * compiled from the given file using NVCC. The name of the
+     * PTX file is returned.
+     *
+     * @param cuFileName The name of the .CU file
+     * @return The name of the PTX file
+     * @throws IOException If an I/O error occurs
+     */
+    private static String preparePtxFile(String cuFileName) throws IOException
+    {
+        int endIndex = cuFileName.lastIndexOf('.');
+        if (endIndex == -1)
+        {
+            endIndex = cuFileName.length()-1;
+        }
+        String ptxFileName = cuFileName.substring(0, endIndex+1)+"ptx";
+        File ptxFile = new File(ptxFileName);
+        if (ptxFile.exists())
+        {
+            return ptxFileName;
+        }
+
+        File cuFile = new File(cuFileName);
+        if (!cuFile.exists())
+        {
+            throw new IOException("Input file not found: "+cuFileName);
+        }
+        String modelString = "-m"+System.getProperty("sun.arch.data.model");
+        String command =
+            "nvcc " + modelString + " -ptx "+
+            cuFile.getPath()+" -o "+ptxFileName;
+
+        System.out.println("Executing\n"+command);
+        Process process = Runtime.getRuntime().exec(command);
+
+        String errorMessage =
+            new String(toByteArray(process.getErrorStream()));
+        String outputMessage =
+            new String(toByteArray(process.getInputStream()));
+        int exitValue = 0;
+        try
+        {
+            exitValue = process.waitFor();
+        }
+        catch (InterruptedException e)
+        {
+            Thread.currentThread().interrupt();
+            throw new IOException(
+                "Interrupted while waiting for nvcc output", e);
+        }
+
+        if (exitValue != 0)
+        {
+            System.out.println("nvcc process exitValue "+exitValue);
+            System.out.println("errorMessage:\n"+errorMessage);
+            System.out.println("outputMessage:\n"+outputMessage);
+            throw new IOException(
+                "Could not create .ptx file: "+errorMessage);
+        }
+
+        System.out.println("Finished creating PTX file");
+        return ptxFileName;
+    }
+    
+    /**
+     * Fully reads the given InputStream and returns it as a byte array
+     *
+     * @param inputStream The input stream to read
+     * @return The byte array containing the data from the input stream
+     * @throws IOException If an I/O error occurs
+     */
+    private static byte[] toByteArray(InputStream inputStream)
+        throws IOException
+    {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte buffer[] = new byte[8192];
+        while (true)
+        {
+            int read = inputStream.read(buffer);
+            if (read == -1)
+            {
+                break;
+            }
+            baos.write(buffer, 0, read);
+        }
+        return baos.toByteArray();
+    }
 
 }
