432996c2aa878f2a6e3782ce6fdc8d6452d86358
Bohdan Benovskyi <benovskiy1994@gmail.com>
2018-05-24 17:08:57 +0300

    Kernel.cu (size: 5*5)

--- src/com/benovskyi/bohdan/Kernel.cu
+++ src/com/benovskyi/bohdan/Kernel.cu
@@ -2,6 +2,6 @@ extern "C"
 __global__ void matrixAdd(int *a, int *b, int *c) {
 	int i = threadIdx.x;
 	int j = threadIdx.y;
-	int index = i + j * N;
+	int index = i + j * 5;
 	c[index] = (a[index] + b[index]) * 5 + 8;
 }
